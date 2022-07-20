bdecb707d0abe33268b34ae4e8db7c58395e5920
jinwang <wj19930703@gmail.com>
2022-07-20 17:54:32 +0200

    two spaceships are set up into the window

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import os.path
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+import sys
+from pygame.locals import *
+
+WIDTH, HEIGHT = 500, 900
+WIN = pygame.display.set_mode((HEIGHT, WIDTH))
+pygame.display.set_caption("Game For 2_GG")
+
+WHITE = (255, 255, 255)
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+FPS = 60
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('assets', 'spaceship_yellow.png'))  # "assets/spaceship_yellow.png"
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(
+    pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load("assets/spaceship_red.png")
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(
+    pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def draw_window():
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP_IMAGE, (300, 100))
+    WIN.blit(RED_SPACESHIP_IMAGE, (700, 100))
+    pygame.display.update()
+    pass
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        draw_window()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
